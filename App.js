c47db638ca71c9a3da5621b6f24520698bc3657a
Imsoup <imre.detar@gmail.com>
2023-06-28 09:21:02 +0200

    updated .gitignore

--- App.js
+++ App.js
@@ -16,12 +16,14 @@
 ReactElement(Object) => HTML(Browser Understands)
 
 */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h1", {}, "This is namaste React"),
     React.createElement("h2", {}, "I am an h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
